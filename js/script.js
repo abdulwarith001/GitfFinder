d262dff20d9596d5927e169b7786d32d32625a76
abdulwarith001 <adebayoayomide646@gmail.com>
2022-09-17 15:56:40 -0800

    Compressed codes into fewer lines of codes

--- js/script.js
+++ js/script.js
@@ -7,53 +7,71 @@ let followingPage = 1;
 
 const getUserDetails = async() => {
   try {
+    const followerURL = `${url}${input.value.toLowerCase()}/followers`;
+    const followingRL = `${url}${input.value.toLowerCase()}/following`;
     followingPage = 1
      const { data } = await axios.get(`${url}${input.value.toLowerCase()}`);
-     const allFollowers = await getFollowers()
-     const allFollowing = await getFollowing()
+     const allFollowers = await getFollows(followerURL)
+     const allFollowing = await getFollows(followingRL);
     const { avatar_url, name, login, bio, email, location, company, twitter_username, followers, following, public_repos } = data;
     
        const txt = `<div class="flex flex-col items-center w-full gap-2">
                 <img src="${avatar_url}" class="w-28 h-28 rounded-md border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${name || login}</p>
+                <p class="text-md text-zinc-300 font-bold font-poppins">${
+                  name || login
+                }</p>
             </div>
             <div class="flex flex-col font-poppins mt-4 gap-2 bg-lightBlack p-4 rounded-xl md:w-full">
-                <h3 class="text-sky-500 text-lg text-center font-bold italic underline">Basic Info</h3>
+                <h3 class="text-sky-500 text-lg text-center font-bold font-poppins">Basic Info</h3>
                 <div class=" flex items-center gap-2 text-zinc-300">
                     <p class="">Bio:</p>
                     <h5 class="font-bold text-md">${bio || "Not avaliable"}</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">Location:</p>
-                    <h5 class="font-bold text-md">${location || "Not avaliable" }</h5>
+                    <h5 class="font-bold text-md">${
+                      location || "Not avaliable"
+                    }</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">E-mail:</p>
-                    <h5 class="font-bold text-md">${email || "Not avaliable"}</h5>
+                    <h5 class="font-bold text-md">${
+                      email || "Not avaliable"
+                    }</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">Twitter:</p>
-                    <h5 class="font-bold text-md">${twitter_username || "Not avaliable"}</h5>
+                    <h5 class="font-bold text-md">${
+                      twitter_username || "Not avaliable"
+                    }</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">Company:</p>
-                    <h5 class="font-bold text-md">${company || "Not avaliable"}</h5>
+                    <h5 class="font-bold text-md">${
+                      company || "Not avaliable"
+                    }</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">Followers:</p>
-                    <h5 class="font-bold text-md">${followers.toLocaleString("en-US") || "Not avaliable"}</h5>
+                    <h5 class="font-bold text-md">${
+                      followers.toLocaleString("en-US") || "Not avaliable"
+                    }</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">Following:</p>
-                    <h5 class="font-bold text-md">${ following.toLocaleString("en-US") || "Not avaliable"}</h5>
+                    <h5 class="font-bold text-md">${
+                      following.toLocaleString("en-US") || "Not avaliable"
+                    }</h5>
                 </div>
                 <div class="flex items-end gap-2 text-zinc-300">
                     <p class="">Public repository:</p>
-                    <h5 class="font-bold text-md">${public_repos || "Not avaliable"}</h5>
+                    <h5 class="font-bold text-md">${
+                      public_repos || "Not avaliable"
+                    }</h5>
                 </div>
             </div>
                 <div class="flex flex-col font-poppins mt-4 gap-4 bg-lightBlack p-4 rounded-xl md:w-full">
-                <h3 class="text-sky-500 text-lg text-center font-bold italic underline">Followers</h3>
+                <h3 class="text-sky-500 text-lg text-center font-bold font-poppins">Followers</h3>
                 <div class="flex flex-col gap-4" id="followersWrapper">${allFollowers}</div>
                  <div class="flex gap-2 justify-center">
                     <button onclick="previousPages(this)" class="p-2 bg-slate-900 hover:bg-slate-500 text-white rounded-md">Prev</button>
@@ -61,7 +79,7 @@ const getUserDetails = async() => {
                 </div>
             </div>
             <div class="flex flex-col font-poppins mt-4 gap-4 bg-lightBlack p-4 rounded-xl md:w-full">
-                <h3 class="text-sky-500 text-lg text-center font-bold italic underline">Following</h3>
+                <h3 class="text-sky-500 text-lg text-center font-bold font-poppins ">Following</h3>
                 <div class="flex flex-col gap-4" id="followingWrapper">${allFollowing}</div>
                  <div class="flex gap-2 justify-center">
                     <button onclick="previousFollowing(this)" class="p-2 bg-slate-900 hover:bg-slate-500 text-white rounded-md">Prev</button>
@@ -76,45 +94,31 @@ const getUserDetails = async() => {
    }
 }
 
-
-const getFollowers = async () => {
-  const { data } = await axios(`${url}${input.value.toLowerCase()}/followers`);
-   if (data.length === 0) {
-     return `<p class="text-white text-md text-center my-2">No followers data was gotten.</p>`;
-   }
-  const paginatedData = pagination(data);
-  const followers = paginatedData.map(follower => {
-    const { login, avatar_url } = follower;
-    const txt = `<div class="flex items-center w-full gap-2">
-                    <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                    <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
+// template for follow data
+const followTemplate = (avatar, username) => {
+  const txt = `<div class="flex items-center w-full gap-2">
+                    <img src="${avatar}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
+                    <p class="text-md text-zinc-300 font-bold font-poppins">${username}</p>
                   </div>`;
-      return txt
-  })
- 
-  const spiltFollowersArr = followers.join(' ')
-  return spiltFollowersArr
+  return txt;
 }
-const getFollowing = async () => {
-  const { data } = await axios(`${url}${input.value.toLowerCase()}/following`);
-  
+
+const getFollows = async (url) => {
+  const { data } = await axios(url);
+
   if (data.length === 0) {
-    return `<p class="text-white text-md text-center my-2">No following data was gotten.</p>`;
-   
+    return `<p class="text-white text-md text-center my-2">No followers data was gotten.</p>`;
   }
+
   const paginatedData = pagination(data);
-  const following = paginatedData.map(following => {
-    const { login, avatar_url } = following;
-    const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-      return txt
-  })
- 
-  const spiltFollowingArr = following.join(' ')
-  return spiltFollowingArr
-}
+  const follows= paginatedData.map((follow) => {
+    const { login, avatar_url } = follow;
+    const template = followTemplate(avatar_url, login);
+    return template;
+  });
+
+  return follows.join(" ");
+};
 
 function pagination(data) {
   currentPage = 1
@@ -140,11 +144,8 @@ async function nextPages(e) {
      currentPage = 1
      const displayPage = page.map((follower) => {
        const { login, avatar_url } = follower;
-       const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-       return txt;
+          const template = followTemplate(avatar_url, login);
+          return template;
      });
      // console.log(activePage);
      followersWrapper.innerHTML = displayPage.join(" ");
@@ -161,11 +162,8 @@ async function nextPages(e) {
  }
   const displayPage = activePage.map(follower => {
     const { login, avatar_url } = follower;
-    const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-    return txt
+     const template = followTemplate(avatar_url, login);
+     return template;
   })
   followersWrapper.innerHTML = displayPage.join(" ");
   return activePage;
@@ -183,11 +181,8 @@ async function previousPages(e) {
     currentPage = 1
       const displayPage = page.map(follower => {
     const { login, avatar_url } = follower;
-    const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-    return txt
+      const template = followTemplate(avatar_url, login);
+      return template;
   })
   followersWrapper.innerHTML = displayPage.join(" ");
   return 
@@ -205,11 +200,8 @@ async function previousPages(e) {
    }
   const displayPage = activePage.map(follower => {
     const { login, avatar_url } = follower;
-    const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-    return txt
+      const template = followTemplate(avatar_url, login);
+      return template;
   })
   followersWrapper.innerHTML = displayPage.join(" ");
   return activePage;
@@ -234,11 +226,8 @@ async function nextFollowing(e) {
     followingPage = 1;
     const displayPage = page.map((follower) => {
       const { login, avatar_url } = follower;
-      const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-      return txt;
+        const template = followTemplate(avatar_url, login);
+        return template;
     });
     followersWrapper.innerHTML = displayPage.join(" ");
     return;
@@ -254,11 +243,8 @@ async function nextFollowing(e) {
   }
   const displayPage = activePage.map((follower) => {
     const { login, avatar_url } = follower;
-    const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-    return txt;
+    const template = followTemplate(avatar_url, login);
+    return template;
   });
   followersWrapper.innerHTML = displayPage.join(" ");
   return activePage;
@@ -278,11 +264,8 @@ async function previousFollowing(e) {
     followingPage = 1;
     const displayPage = page.map((follower) => {
       const { login, avatar_url } = follower;
-      const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-      return txt;
+       const template = followTemplate(avatar_url, login);
+       return template;
     });
     followersWrapper.innerHTML = displayPage.join(" ");
     return;
@@ -300,15 +283,14 @@ async function previousFollowing(e) {
   }
   const displayPage = activePage.map((follower) => {
     const { login, avatar_url } = follower;
-    const txt = `<div class="flex items-center w-full gap-2">
-                <img src="${avatar_url}" class="w-16 h-16 rounded-full border-2 border-slate-100" alt="Github avatar">
-                <p class="text-md text-zinc-300 font-bold font-poppins">${login}</p>
-            </div>`;
-    return txt;
+     const template = followTemplate(avatar_url, login);
+     return template;
   });
   followersWrapper.innerHTML = displayPage.join(" ");
   return activePage;
 }
+
+
 
 input.addEventListener('keyup', (e) => {
    if (e.key === 'Enter') {
